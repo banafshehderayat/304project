2473374c8aaf7f20e2109392508f8af4ac5ffc69
Grace Liang <acie.liang@gmail.com>
2016-03-12 18:07:31 -0800

    Can add customers. Have debug printing

--- main.php
+++ main.php
@@ -43,14 +43,56 @@ function executeBoundSQL($cmdstr, $list) {
 			$success = False;
 		}
 	}
+}
+
+function executePlainSQL($cmdstr) { //takes a plain (no bound variables) SQL command and executes it
+	//echo "<br>running ".$cmdstr."<br>";
+	global $db_conn, $success;
+	$statement = OCIParse($db_conn, $cmdstr); //There is a set of comments at the end of the file that describe some of the OCI specific functions and how they work
+
+	if (!$statement) {
+		echo "<br>Cannot parse the following command: " . $cmdstr . "<br>";
+		$e = OCI_Error($db_conn); // For OCIParse errors pass the       
+		// connection handle
+		echo htmlentities($e['message']);
+		$success = False;
+	}
+
+	$r = OCIExecute($statement, OCI_DEFAULT);
+	if (!$r) {
+		echo "<br>Cannot execute the following command: " . $cmdstr . "<br>";
+		$e = oci_error($statement); // For OCIExecute errors pass the statementhandle
+		echo htmlentities($e['message']);
+		$success = False;
+	} else {
+
+	}
+	return $statement;
+}
+
+function printResult($result) { //prints results from a select statement
+	echo "<br>Got data from table customers:<br>";
+	echo "<table>";
+	echo "<tr><th>cname</th><th>address</th></tr>";
+
+	while ($row = OCI_Fetch_Array($result, OCI_BOTH)) {
+		// Row indices MUST BE IN CAPS
+		echo "<tr><td>" . $row["CNAME"] . "</td><td>" . $row["ADDRESS"] . "</td></tr>"; //or just use "echo $row[0]" 
+	}
+	echo "</table>";
 
 }
 
 
-
+$debug = True;
 $db_conn = OCILogon("ora_b9y8", "a38319125", "ug");
 if ($db_conn) {
-	echo "Successfully connected to Oracle. \n";
+	
+	if ($debug) {
+		echo "Successfully connected to Oracle. \n";
+		$result = executePlainSQL("select * from customers");
+		printResult($result);
+	}
 
 	if (array_key_exists('addCust', $_POST)) {
 		$tuple = array (
