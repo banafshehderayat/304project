f6cdbcccb0ce7fcacbe8dc96cbe92fa55ac1ae93
banafshehderayat <b.derayat@gmail.com>
2016-03-24 13:27:27 -0700

    fixed selecting customers

--- employee.php
+++ employee.php
@@ -1,60 +1,65 @@
-
 <html>
 <form method="POST" action="employee.php">
-	Customer Name: <input type="text" name="custName"> <br>
-	Customer Address: <input type="text" name="custAddr"> <br>
-	<input type="submit" value="Find Customer" name="findCust"></p>
+        Customer Name: <input type="text" name="custName"> <br>
+        Customer Address: <input type="text" name="custAddr"> <br>
+        <input type="submit" value="Find Customer" name="findCust"></p>
+        <br>
+        <br>
+
+        Room Location: <input type="text" name="roomLoc"> <br>
+        <input type="submit" value="View Rooms" name="viewRooms"></p>
+
+        Start Date : <input type="date" name="startDate"><br>
+        End Date : <input type="date" name="endDate"><br>
+        <input type="submit" value="Find Available Rooms"
+name="findRooms"></p>
+
 </form>
 
 
-<?php 
+<?php
 error_reporting(-1);
 ini_set('display_errors',1);
 
 require 'util.php';
 $util = new Util;
-$debug = False;
+$debug = True;
 
 $db_conn = OCILogon("ora_b9y8", "a38319125", "ug");
 if ($db_conn) {
-	
-	if ($debug) {
-		echo "Successfully connected to Oracle. \n";
-	}
+        if ($debug) {
+                echo "Successfully connected to Oracle. \n";
+        }
 
-	if (array_key_exists('findCust', $_POST)) {
-		$tuple = array (
-				":bind1" => $_POST['custName'],
-				":bind2" => $_POST['custAddr']
-			);
-		$allTuple = array (
-			$tuple
-		);
-		
-		$customers = $util->executeBoundSQL("select * FROM customers
-					WHERE CNAME = (:bind1) AND 
-					ADDRESS = (:bind2)", $allTuple);
-		
-		OCICommit($db_conn);
-		$util->printResult($customers);
+        if (array_key_exists('findCust', $_POST)) {
+                $statement = 'SELECT * FROM customers WHERE cname =
+:bind1 and address = :bind2';
+                $stid = oci_parse($db_conn, $statement);
+                $bind1 = $_POST['custName'];
+                $bind2 = $_POST['custAddr'];
+                OCIBindByName($stid, ':bind1', $bind1);
+                OCIBindByName($stid, ':bind2', $bind2);
+                $customer = OCIExecute($stid);
+                echo('this is customer ' + $customer);
+                $util->printResult($stid);
+                OCICommit($db_conn);
+                if ($debug) {
+                        echo "Customer found\n";
+                }
+        }
 
 
-		if ($debug) { 
-			echo "Customer added\n";
-		}
-	}
+        if ($debug) {
+                echo "good\n";
+        }
 
-	if ($debug) {
-		$result = $util->executePlainSQL("select * from customers");
-		$util->printResult($result);
-	}
-
-	OCILogoff($db_conn);
+        OCILogoff($db_conn);
 }
 else {
-	$err = OCIError();
-	echo "Oracle Connect Error" . $err['message'];
+        $err = OCIError();
+        echo "Oracle Connect Error" . $err['message'];
 }
 
 ?>
 </html>
+
