1a083bc5f840b5411bbfda0c734b27ebb993504b
banafshehderayat <b.derayat@gmail.com>
2016-04-02 19:46:27 -0700

    added errors to manager

--- manager.php
+++ manager.php
@@ -7,8 +7,7 @@
 		  			require_once 'util.php';
 		  			$util2 = new Util;
 					$debug = False;
-					if ($debug) {
-					}
+
 		  			$db_conn = OCILogon("ora_j7l8", "a31501125", "ug");
 					if ($db_conn) {
 						$result = $util2->executePlainSQL("select * from location");
@@ -18,12 +17,24 @@
 				?>
 			</select>
 		Manager ID: <input type="text" name="manId">
-		Password: <input type="text" name="pass">
+		Password: <input type="text" name="pass" >
         	<input type="submit" value="Add Employee" name="addEmployee"></p>		
 	</div>
 	<div style="border: 1px solid black;">
-		Employee ID: <input type="text" name="eID">
-        	<input type="submit" value="Find Employee" name="findEmployee"></p>		
+		Employee ID:<select name="eID">
+		  		<?php 
+		  			require_once 'util.php';
+		  			$util2 = new Util;
+					$debug = True;
+		  			$db_conn = OCILogon("ora_j7l8", "a31501125", "ug");
+					if ($db_conn) {
+						$result = $util2->executePlainSQL("select employee_id from employee");
+						$util2->printResultDropdown($result, 'EMPLOYEE_ID');
+						OCILogoff($db_conn);
+					}
+				?>
+			</select>
+        	<input type="submit" value="Find Employee" name="findEmployee" ></p>		
 	</div>
 	
 		<br>
@@ -54,7 +65,7 @@
 	</form>
 
 	<?php
-		error_reporting(-1);
+		error_reporting(E_ERROR);
 		ini_set('display_errors',1);
 
 		require_once 'util.php';
@@ -78,50 +89,55 @@
 					$statement = "SELECT employee_id, name, location_address, manager_id 
 							  	  FROM employee 
 							  	  WHERE EMPLOYEE_ID = :bind";
-                			$stid = oci_parse($db_conn, $statement);
-                			$bind = $_POST['eID'];
-               				OCIBindByName($stid, ':bind', $bind);
-                			OCIExecute($stid);
+        			$stid = oci_parse($db_conn, $statement);
+        			$bind = $_POST['eID'];
+       				OCIBindByName($stid, ':bind', $bind);
+        			OCIExecute($stid);
 					
 					$employee = OCI_Fetch_Array($stid);
+                    if ($employee == FALSE){
+                        echo '<script type="text/javascript">'. 'alert("The employee ID you have entered is incorrect.")</script>';
+                    } else {
+    					// Get manager name
+    					$manStatement = "SELECT name
+    									 FROM employee
+    									 WHERE employee_id = :manID";
+    					$st = oci_parse($db_conn, $manStatement);
+    					OCIBindByName($st, ':manID', $employee["MANAGER_ID"]);
+    					OCIExecute($st);
+    					$manager = OCI_Fetch_Array($st);
 
-					// Get manager name
-					$manStatement = "SELECT name
-									 FROM employee
-									 WHERE employee_id = :manID";
-					$st = oci_parse($db_conn, $manStatement);
-					OCIBindByName($st, ':manID', $employee["MANAGER_ID"]);
-					OCIExecute($st);
-					$manager = OCI_Fetch_Array($st);
+    					echo '<form name="form1" method="post" action="">';
+    					echo "<table>";
 
-					echo '<form name="form1" method="post" action="">';
-					echo "<table>";
-
-        			echo "<tr><th>Employee ID</th><th>Name</th><th>Location Address</th><th>Manager Name</th></tr>";
-					echo "<tr><td><input type='text' name='eid' value='" . $employee["EMPLOYEE_ID"] . "' readonly></td>
-						      <td><input type='text' name='name' value='" . $employee["NAME"] . "'></td>
-						      <td><input type='text'name='loc' value='" .$employee["LOCATION_ADDRESS"]."'></td>
-						      <td><input type='text'name='manName' value='" .$manager["NAME"]."' readonly></td>
-						  </tr>"; 
-					
-        			echo "</table>";
-					echo "<br>";
-					echo "<input type='submit' value='update' name='update'>";
-					echo "<input type='submit' value='delete' name='delete'>";
-					echo "</form>";
-                	OCICommit($db_conn);
+            			echo "<tr><th>Employee ID</th><th>Name</th><th>Location Address</th><th>Manager Name</th></tr>";
+    					echo "<tr><td><input type='text' name='eid' value='" . $employee["EMPLOYEE_ID"] . "' readonly></td>
+    						      <td><input type='text' name='name' value='" . $employee["NAME"] . "'></td>
+    						      <td><input type='text'name='loc' value='" .$employee["LOCATION_ADDRESS"]."'></td>
+    						      <td><input type='text'name='manName' value='" .$manager["NAME"]."' readonly></td>
+    						  </tr>"; 
+    					
+            			echo "</table>";
+    					echo "<br>";
+    					echo "<input type='submit' value='update' name='update'>";
+    					echo "<input type='submit' value='delete' name='delete'>";
+    					echo "</form>";
+                    	OCICommit($db_conn);
+				    }
 				} else  
 					if (array_key_exists('update', $_POST)){
 						
 							$statement = "UPDATE employee SET NAME = :bind1 , LOCATION_ADDRESS = :bind2 
 							WHERE EMPLOYEE_ID = :bind3";
 							$stid = oci_parse($db_conn, $statement);
+							
 							$bind1 = $_POST['name'];
                 			$bind2 = $_POST['loc'];
 							$bind3 = $_POST['eid'];
                 			OCIBindByName($stid, ':bind1', $bind1);
                 			OCIBindByName($stid, ':bind2', $bind2);
 							OCIBindByName($stid, ':bind3', $bind3);
+
                				$success = @OCIExecute($stid);
 							
 							if ($success) {
@@ -162,8 +178,12 @@
 								OCIBindByName($stid, ':bind2', $bind2);
 								OCIBindByName($stid, ':bind3', $bind3);
 								OCIBindByName($stid, ':bind4', $bind4);
-								OCIExecute($stid);
-								OCICommit($db_conn);	
+								$r = OCIExecute($stid);
+								if ($r == FALSE){
+									echo '<script type="text/javascript">'. 'alert("The employee information you have entered is invalid.")</script>';
+								} else{ OCICommit($db_conn);
+									}
+									
 							} else 
 								if(array_key_exists ('findCust', $_POST)){
 									$statement = "SELECT cname FROM customers where NOT EXISTS ((select room_number from location Natural JOIN rooms where location_address=:bind1) MINUS (select room_number from reserves where reserves.name = customers.cname and reserves.address = customers.address))";
